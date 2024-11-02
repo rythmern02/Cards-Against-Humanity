f64d5b3ebe581db346dc470e9b4e42a5d6a3760e
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2024-11-02 13:38:45 +0530

    some minor changes in actions.json

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -3,20 +3,9 @@ import { ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
 export const GET = async () => {
   const payload: ActionsJson = {
     rules: [
-      // map all root level routes to an action
       {
-        pathPattern: "/*",
+        pathPattern: "/",
         apiPath: "/api/actions/*",
-      },
-      // idempotent rule as the fallback
-      /*
-      Idempotent rules allow blink clients to more easily determine if a given path supports 
-      Action API requests without having to be prefixed with the solana-action: 
-      URI or performing additional response testing.
-      */
-      {
-        pathPattern: "/api/actions/**",
-        apiPath: "/api/actions/**",
       },
     ],
   };
